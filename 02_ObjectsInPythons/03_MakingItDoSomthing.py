25e724195b7d557c8c1fddeb281282951dd55e44
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-10-02 20:38:18 +0530

    Making it do something 02

--- 02_ObjectsInPythons/03_MakingItDoSomthing.py
+++ 02_ObjectsInPythons/03_MakingItDoSomthing.py
@@ -1,8 +1,27 @@
+import math
+
 class Point:
     def reset(self):
         self.x = 0
-        self .y =0
+        self.y =0
 
-p = Point()
-p.reset()
-print(p.x,p.y)
+    def move(self,x,y):
+        self.x = x
+        self.y = y
+
+    def claculate_distance(self,other_point):
+        return math.sqrt((self.x - other_point.x)**2 + (self.y - other_point.y)**2)
+
+point1 = Point()
+point2 = Point()
+
+point1.reset()
+point2.move(5,0)
+
+print(point2.claculate_distance(point1))
+
+assert (point2.claculate_distance(point1) == point1.claculate_distance(point2))
+
+point1.move(3,4)
+print(point1.claculate_distance(point2))
+print(point1.claculate_distance(point1))
