f1df31bdd4bdb0eaa82769319fc606f8590346b3
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-10-03 11:09:49 +0530

    importing modules and packages

---
+++ 02_ObjectsInPythons/parent_directory/ecommerce/database.py
@@ -0,0 +1,9 @@
+class Database:
+    'the databse implimation'
+    pass
+
+database = None
+
+def initialize_database():
+    global database
+    database = Database()
---
+++ 02_ObjectsInPythons/parent_directory/main.py
@@ -0,0 +1,13 @@
+from ecommerce.database import database
+
+class UsefulClass:
+    '''This class might be useful to other module'''
+    pass
+
+def main():
+    '''Create a useful class and does something with it for our module'''
+    useful = UsefulClass()
+    print(useful)
+
+if __name__ == "__main__":
+    main()
