7e0ee9e87f09af55df4f0ee8aa84e58a8730c8ea
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-10-03 07:53:41 +0530

    Initializing the objects 02

--- 02_ObjectsInPythons/04_InitializingTheObject.py
+++ 02_ObjectsInPythons/04_InitializingTheObject.py
@@ -1,13 +1,16 @@
 class Point:
-    def __init__(self,x,y):
+    def __init__(self,x=0,y=0):
         self.move(x,y)
 
     def move(self,x,y):
         self.x = x
         self.y = y
 
-    def reset(self):
-        self.move(0,0)
+    # def reset(self):
+    #     self.move(0,0)
 
-point = Point(3,5)
+point = Point()
 print(point.x,point.y)
+
+point1 = Point(3,5)
+print(point1.x,point1.y)
