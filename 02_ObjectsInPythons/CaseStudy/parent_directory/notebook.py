8083250da2f12aa816eb1586cdcc0cc4edd99761
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-10-04 09:45:29 +0530

    Fix modify_tags and modify_memo with new method _find_note

--- 02_ObjectsInPythons/CaseStudy/parent_directory/notebook.py
+++ 02_ObjectsInPythons/CaseStudy/parent_directory/notebook.py
@@ -36,21 +36,30 @@ class Notebook:
         '''Create a new note and add it to the list.'''
         self.notes.append(Note(memo,tags))
 
+    def _find_note(self,note_id):
+        '''Locate the note with the given id.'''
+        for note in self.notes:
+            if note.id == note_id:
+                return note
+            return None
+
     def modify_memo(self, note_id, memo):
         '''Find the note with the given Id and change its
         memo to given value.'''
-        for note in self.notes:
-            if note.id == note_id:
-                note.memo = memo
-                break
+        # for note in self.notes:
+        #     if note.id == note_id:
+        #         note.memo = memo
+        #         break
+        self._find_note(note_id).memo = memo
 
     def modify_tags(self, note_id, tags):
         '''Find the note with the given id and change its
         tags to the given value.'''
-        for note in self.notes:
-            if note.id == note_id:
-                note.tags = tags
-                break
+        # for note in self.notes:
+        #     if note.id == note_id:
+        #         note.tags = tags
+        #         break
+        self._find_note(note_id).tags = tags
 
     def search(self, filter):
         '''Find all notes that match the given filter string.'''
