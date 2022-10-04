d317e7877e62bc8fb1e5ae54b0405f14cd2b816e
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-10-04 09:08:44 +0530

    Create Notebok class-1

--- 02_ObjectsInPythons/CaseStudy/parent_directory/notebook.py
+++ 02_ObjectsInPythons/CaseStudy/parent_directory/notebook.py
@@ -23,3 +23,35 @@ class Note:
         Return if it matches, False otherwise.
         Search is case sensitive and matches both text and tags.'''
         return filter in self.memo or filter in self.tags
+
+class Notebook:
+    '''Represent a collection of notes that can be taged,
+    modified and search.'''
+
+    def __init__(self):
+        '''Iiniialize a Notebook with a empty list.'''
+        self.notes = []
+
+    def new_note(self, memo, tags=''):
+        '''Create a new note and add it to the list.'''
+        self.notes.append(Note(memo,tags))
+
+    def modify_memo(self, note_id, memo):
+        '''Find the note with the given Id and change its
+        memo to given value.'''
+        for note in self.notes:
+            if note.id == note_id:
+                note.memo = memo
+                break
+
+    def modify_tags(self, note_id, tags):
+        '''Find the note with the given id and change its
+        tags to the given value.'''
+        for note in self.notes:
+            if note.id == note_id:
+                note.tags = tags
+                break
+
+    def search(self, filter):
+        '''Find all notes that match the given filter string.'''
+        return [note for note in self.notes if note.match(filter)]
