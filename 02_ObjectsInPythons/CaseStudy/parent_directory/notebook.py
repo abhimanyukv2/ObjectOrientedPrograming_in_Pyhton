955be6083fcd062147174e2fcf4e936f627506fc
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-10-04 08:38:32 +0530

    Creating note object and initialize with memo, tags, date and Id. define a search filter in it

---
+++ 02_ObjectsInPythons/CaseStudy/parent_directory/notebook.py
@@ -0,0 +1,25 @@
+import datetime
+
+# Store the next available id for all new notes
+id = 0
+
+class Note:
+    '''Represent a note in the notebook. Match against a
+    string in searches and store tags for each notes.'''
+
+    def __init__(self, memo, tags=''):
+        '''Initialize a note with memo and optional
+        space seperated tags. Automatically set the note's
+        creation date and unique id.'''
+        self.memo = memo
+        self.tags = tags
+        self.creation_date = datetime.date.today()
+        global last_id
+        last_id += 1
+        self.id = last_id
+
+    def match(self, filter):
+        '''Determined if this note natches the filter text.
+        Return if it matches, False otherwise.
+        Search is case sensitive and matches both text and tags.'''
+        return filter in self.memo or filter in self.tags
