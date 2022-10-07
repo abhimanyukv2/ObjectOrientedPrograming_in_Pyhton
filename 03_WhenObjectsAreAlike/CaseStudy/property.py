9466000b27e8cd6cd002fd1c55f1df5599d8a094
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-10-07 12:51:19 +0530

    Add Purchse class

--- 03_WhenObjectsAreAlike/CaseStudy/property.py
+++ 03_WhenObjectsAreAlike/CaseStudy/property.py
@@ -113,3 +113,25 @@ class House(Property):
         return parent_init
     
     prompt_init = staticmethod(prompt_init)
+
+class Purchase:
+    def __init__(self, price='', taxes='', **kwargs):
+        super().__init__(**kwargs)
+        self.price = price
+        self.taxes = taxes
+
+    def display(self):
+        super().display()
+        print('''
+            PURCHASE DETAILS
+        ==========================
+            Selling price: {self.price}
+            Estimated taxes: {self.taxes}''')
+
+    def prompt_init():
+        return dict(
+            price = input("What is the selling price? "),
+            taxes = input('What is the estimated taxes? ')
+        )
+
+    prompt_init = staticmethod(prompt_init)
