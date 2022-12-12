17a25b48aeded03fbe1d6ac709e81872ba947c30
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-12-12 10:15:18 +0530

    creating cursor class

--- 05_WhenToUseObjectOrientedProgramming/CaseStudy/document.py
+++ 05_WhenToUseObjectOrientedProgramming/CaseStudy/document.py
@@ -21,3 +21,25 @@ class Document:
     
     def back(self):
         self.cursor -= 1
+
+class Cursor:
+    def __init__(self, document):
+        self.document = document
+        self.position = 0
+
+    def forward(self):
+        self.position += 1
+    
+    def back(self):
+        self.position -= 1
+
+    def home(self):
+        while self.document.characters[self.position - 1] != '\n':
+            self.position -= 1
+            if self.position == 0:
+                # Got to the beginning of file before new line
+                break
+
+    def end(self):
+        while self.position < len(self.document.characters) and self.document.characters[self.position] != '\n':
+            self.position += 1
