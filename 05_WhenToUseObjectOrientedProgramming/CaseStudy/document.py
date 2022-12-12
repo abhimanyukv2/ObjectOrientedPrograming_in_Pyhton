06c113d1c98d628d0180641e0310cff1d5f87f01
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-12-12 11:08:14 +0530

    for checking Character.charater in home and end method of cursor object

--- 05_WhenToUseObjectOrientedProgramming/CaseStudy/document.py
+++ 05_WhenToUseObjectOrientedProgramming/CaseStudy/document.py
@@ -34,14 +34,14 @@ class Cursor:
         self.position -= 1
 
     def home(self):
-        while self.document.characters[self.position - 1].charater != '\n':
+        while self.document.characters[self.position - 1].character != '\n':
             self.position -= 1
             if self.position == 0:
                 # Got to the beginning of file before new line
                 break
 
     def end(self):
-        while self.position < len(self.document.characters) and self.document.characters[self.position].charater != '\n':
+        while self.position < len(self.document.characters) and self.document.characters[self.position].character != '\n':
             self.position += 1
 
 class Character:
