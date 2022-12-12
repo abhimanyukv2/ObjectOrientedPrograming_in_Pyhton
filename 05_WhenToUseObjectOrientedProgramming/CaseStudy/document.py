e29a3577d45f9ff491e8adb125f430ca3fee8a94
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-12-12 10:44:18 +0530

    create character object for bold, underline and italic

--- 05_WhenToUseObjectOrientedProgramming/CaseStudy/document.py
+++ 05_WhenToUseObjectOrientedProgramming/CaseStudy/document.py
@@ -41,3 +41,17 @@ class Cursor:
     def end(self):
         while self.position < len(self.document.characters) and self.document.characters[self.position] != '\n':
             self.position += 1
+
+class Character:
+    def __init__(self, character, bold=False, italic=False, underline=False):
+        assert len(character) == 1
+        self.character = character
+        self.bold = bold
+        self.italic = italic
+        self.underline = underline
+
+    def __str__(self):
+        bold = "*" if self.bold else ''
+        italic = "/" if self.italic else ''
+        underline = "_" if self.underline else ''
+        return bold + italic + underline + self.character
